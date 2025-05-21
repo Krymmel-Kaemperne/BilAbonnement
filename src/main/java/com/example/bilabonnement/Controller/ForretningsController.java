8290877df7a23d2df717a28437c1ff975c6ee4f3
Tobias Dosdal-Feddersen <kontakt@tobiasdosdal.dk>
2025-05-21 11:38:52 +0200

    ensarte og forretnings

---
+++ src/main/java/com/example/bilabonnement/Controller/ForretningsController.java
@@ -0,0 +1,39 @@
+package com.example.bilabonnement.Controller;
+
+import com.example.bilabonnement.Model.ForretningsModel;
+import com.example.bilabonnement.Service.ForretningsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ForretningsController {
+    private final ForretningsService forretningsService;
+
+    @Autowired
+    public ForretningsController(ForretningsService forretningsService) {
+        this.forretningsService = forretningsService;
+    }
+
+    @GetMapping("/forretningsdata/omsætning")
+    public String showOmsaetningDashboard(Model model) {
+        ForretningsModel metrics = forretningsService.getMetrics();
+        model.addAttribute("metrics", metrics);
+        return "businessData/omsætning";
+    }
+
+    @GetMapping("/forretningsdata/fleet")
+    public String showFleetDashboard(Model model) {
+        ForretningsModel metrics = forretningsService.getMetrics();
+        model.addAttribute("metrics", metrics);
+        return "businessData/fleet";
+    }
+
+    @GetMapping("/forretningsdata/andre")
+    public String showAndreDashboard(Model model) {
+        ForretningsModel metrics = forretningsService.getMetrics();
+        model.addAttribute("metrics", metrics);
+        return "businessData/andre";
+    }
+} 
